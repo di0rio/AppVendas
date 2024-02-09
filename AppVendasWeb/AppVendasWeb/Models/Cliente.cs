91e2ea69e2eab43879ea1b43f3069621f75220bc
Cauã Diório <caua.diorio29@gmail.com>
2024-02-09 10:56:25 -0300

    js feito para mascaras do celular e cpf e id adicionados no cpf e celular

--- AppVendasWeb/AppVendasWeb/Models/Cliente.cs
+++ AppVendasWeb/AppVendasWeb/Models/Cliente.cs
@@ -12,7 +12,7 @@ namespace AppVendasWeb.Models
 		public string ClienteNome { get; set; }
 
 		[Required(ErrorMessage = "O campo CPF é obrigatório")]
-		[StringLength(11, ErrorMessage = "O campo Nome deve ter no máximo 11 caracteres")]
+		[StringLength(14, ErrorMessage = "O campo Nome deve ter no máximo 14 caracteres")]
 		public string CPF { get; set; }
 
 		[Display(Name = "E-mail")]
@@ -24,6 +24,7 @@ namespace AppVendasWeb.Models
 		[Required(ErrorMessage = "A Data de Nascimento é obrigatória")]
 		public DateOnly DataNascimento { get; set; }
 
+		[StringLength(15, ErrorMessage = "O campo Nome deve ter no máximo 15 caracteres")]
 		public string? Celular { get; set; }
 
 		[Display(Name = "Data de Cadastro")]
